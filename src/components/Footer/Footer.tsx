ca4096cc4b1b024205e52de0ae99c78f08af49e2
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-01-24 17:07:57 -0300

    insert components to footer section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,11 @@
+import { Button } from '../Button'
+import { Text } from '../Footer/Text'
+
 export function Footer() {
     return (
-        <div></div>
+        <div className="bg-ft bg-cover bg-no-repeat bg-center w-full h-[800px] flex flex-col items-center justify-center">
+            <Text />
+            <Button />
+        </div>
     )
 }
