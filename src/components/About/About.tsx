762ebfdf6c9f569e5a0a8f6e03be52b8e4539856
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-01-25 22:37:02 -0300

    u

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import { Button } from '../Button'
 
 export function About() {
     return (
-        <div className="h-[500px] sm:h-[600px] sm:w-[1300px] md:w-[1000px] sm:flex items-center justify-between">
+        <div className="h-[500px] sm:h-[600px] sm:w-[1000px] md:w-[1200px] sm:flex items-center justify-between">
             <img src={iphone} className="w-[300px] mt-5 sm:w-[500px] sm:h-[340px] rounded-xl sm:rounded-tl-[210px] sm:rounded-tr-[44px] sm:rounded-br-[210px] sm:rounded-bl-[44px]" />
 
             <div className="flex flex-col items-center justify-center gap-[30px] m-10 sm:gap-[70px] ">
